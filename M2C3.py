a252a947a0c0af6a75e2c474189773a003033401
BYANP <peredadam@gmail.com>
2026-03-01 13:31:34 +0100

    M2C3

---
+++ M2C3.py
@@ -0,0 +1,57 @@
+# Ejercicio 1
+
+texto = "Hola mundo"
+numero = 10
+lista = [1, 2, 3]
+booleano = True
+
+print(texto)
+print(numero)
+print(lista)
+print(booleano)
+
+# Ejercicio 2
+
+sub_texto = texto[:3]
+
+print(sub_texto)
+
+# Ejercicio 3
+
+primer_elemento = lista[0]
+
+print(primer_elemento)
+
+#Ejercicio 4
+
+sumar_diez = numero + 10
+
+print(sumar_diez)
+
+#Ejercicio 5
+
+fin_texto = lista[-1]
+
+print(fin_texto)
+
+# Ejercicio 6
+
+names = 'harry,alex,susie,jared,gail,conner'
+lista_nombres = names.split(',')
+
+print(lista_nombres)
+
+#Ejercicio 7
+
+primera_palabra = lista_nombres[0] 
+lista_primera_mayus = primera_palabra.upper(),lista_nombres[1:]
+
+print(lista_primera_mayus)
+
+# Ejercicio 8
+
+print(f"El numero {sumar_diez}")
+
+#Ejercio 9
+
+print("hello world")
